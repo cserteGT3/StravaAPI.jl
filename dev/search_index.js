3d05abc3bb1ba925a60ca296b9932e529b2c7802
Documenter.jl <documenter@juliadocs.github.io>
2024-07-15 21:10:56 +0000

    build based on a338aa6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StravaAPI","category":"page"},{"location":"#StravaAPI","page":"Home","title":"StravaAPI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StravaAPI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StravaAPI]","category":"page"}]
+}
